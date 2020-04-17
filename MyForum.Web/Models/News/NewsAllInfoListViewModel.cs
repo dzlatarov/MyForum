d7a068d06cc2aa83bfb704c806ad7d82b81f9bf6
dzlatarov <danzlatarov1@gmail.com>
2020-04-17 17:42:27 +0300

    create all news view

---
+++ MyForum.Web/Models/News/NewsAllInfoListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyForum.Web.Models.News
+{
+    public class NewsAllInfoListViewModel
+    {
+        public List<NewsAllInfoViewModel> News { get; set; }
+    }
+}
