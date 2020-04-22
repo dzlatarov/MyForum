dd599bd66142b157c07c16d033f05a8d53bd3672
dzlatarov <danzlatarov1@gmail.com>
2020-04-22 21:32:01 +0300

    check threads count should be 0

--- MyForum.Tests/UsersServiceTests.cs
+++ MyForum.Tests/UsersServiceTests.cs
@@ -5,6 +5,8 @@ using Xunit;
 using Moq;
 using System.Threading.Tasks;
 using MyForum.Domain;
+using MyForum.Services;
+using System.Linq;
 
 namespace MyForum.Tests
 {
@@ -21,6 +23,20 @@ namespace MyForum.Tests
             Assert.Equal(expected, mockUser.Object.IsDeactivate);
         }
 
-        //public async Task Check
+        [Fact]
+        public async Task CheckThreadCount_shouldBe0()
+        {
+            var mockUser = new Mock<ApplicationUser>();
+            mockUser.Setup(u => u.UserName).Returns("pesho");
+
+            var mockUsersService = new Mock<IUsersService>();
+            mockUsersService.Setup(u => u.GetUserByUsername(mockUser.Object.UserName));
+
+
+            var threadCreatedCountByUser = mockUser.Object.Threads.Count;
+            var expected = 0;
+
+            Assert.Equal(expected, threadCreatedCountByUser);
+        }
     }
 }
