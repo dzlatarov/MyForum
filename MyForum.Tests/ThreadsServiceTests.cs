d30b819db178b88577a2d57b563a492cbbd48c50
dzlatarov <danzlatarov1@gmail.com>
2020-04-25 14:16:26 +0300

    check if get thread by id returns correct thread

---
+++ MyForum.Tests/ThreadsServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyForum.Domain;
+using MyForum.Persistence;
+using MyForum.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyForum.Tests
+{
+    public class ThreadsServiceTests
+    {
+        [Fact]
+        public async Task CheckIfGetThreadByIdReturnsCorrectThread()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<MyForumDbContext>()
+              .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var context = new MyForumDbContext(optionBuilder.Options);
+            var usersService = new UsersService(context);
+            var categoriesService = new CategoriesService(context);
+            var threadsService = new ThreadsService(context, usersService, categoriesService);
+
+            var thread = new Thread
+            {
+                Id = "112233",
+                Title = "some title",
+                Content = "some content",
+                CreatedOn = DateTime.UtcNow,
+                ThreadCreatorId = "1122",
+                CategoryId = "2211"
+            };
+
+            await context.Threads.AddAsync(thread);
+            await context.SaveChangesAsync();
+
+            var expected = thread;
+            var actual = threadsService.GetThreadById(thread.Id).Result;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
