0e6720f80453c3d767bd7ccfb21044724f408590
dzlatarov <danzlatarov1@gmail.com>
2020-04-25 14:53:12 +0300

    check if edit method changes correctly

--- MyForum.Tests/CommentsServiceTests.cs
+++ MyForum.Tests/CommentsServiceTests.cs
@@ -75,5 +75,40 @@ namespace MyForum.Tests
 
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public async Task CheckIfEditMethodChangesCorrectly()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<MyForumDbContext>()
+             .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var context = new MyForumDbContext(optionBuilder.Options);
+            var usersService = new UsersService(context);
+            var categoriesService = new CategoriesService(context);
+            var threadsService = new ThreadsService(context, usersService, categoriesService);
+            var commentsService = new CommentsService(context, threadsService, usersService);
+
+            var comment = new Comment
+            {
+                Id = "123456",
+                Content = "some content",
+                ThreadId = "1122",
+                CommentCreatorId = "3344",
+                CreatedOn = DateTime.UtcNow,
+                Quote = "some quote"
+            };
+
+            context.Comments.Add(comment);
+            context.SaveChanges();
+
+            var content = "some content";
+            var createdOn = DateTime.UtcNow;
+
+            await commentsService.Edit("123456", "only content", DateTime.Now);
+            var commentFromDb = await commentsService.GetCommentById("123456");
+
+            Assert.True(content != commentFromDb.Content);
+            Assert.True(createdOn != commentFromDb.CreatedOn);
+        }
     }
 }
