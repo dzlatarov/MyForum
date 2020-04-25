72b0450be68a63efe032c7b6574632d8f8c5c492
dzlatarov <danzlatarov1@gmail.com>
2020-04-25 14:28:54 +0300

    check if Edit method changes correctly

--- MyForum.Tests/ThreadsServiceTests.cs
+++ MyForum.Tests/ThreadsServiceTests.cs
@@ -73,5 +73,39 @@ namespace MyForum.Tests
 
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public async Task CheckIfEditMethodChangesCorrectly()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<MyForumDbContext>()
+              .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var context = new MyForumDbContext(optionBuilder.Options);
+            var usersService = new UsersService(context);
+            var categoriesService = new CategoriesService(context);
+            var threadsService = new ThreadsService(context, usersService, categoriesService);
+
+            var thread = new Thread
+            {
+                Id = "112233",
+                Title = "some title",
+                Content = "some content",
+                CreatedOn = DateTime.UtcNow,
+                ThreadCreatorId = "1122",
+                CategoryId = "2211"
+            };
+
+            await context.Threads.AddAsync(thread);
+            await context.SaveChangesAsync();
+
+            var title = thread.Title;
+            var content = thread.Content;
+
+            await threadsService.Edit("112233", "only title", "only content", DateTime.Now);
+            var threadFromDb = threadsService.GetThreadById("112233").Result;
+
+            Assert.True(title != threadFromDb.Title);
+            Assert.True(content != threadFromDb.Content);
+        }
     }
 }
