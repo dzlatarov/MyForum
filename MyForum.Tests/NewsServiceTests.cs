e673a10d42f7d09cb68e5a7e7a748133e2db8825
dzlatarov <danzlatarov1@gmail.com>
2020-04-25 15:00:42 +0300

    check if delete method return correct count of news

---
+++ MyForum.Tests/NewsServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyForum.Domain;
+using MyForum.Persistence;
+using MyForum.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyForum.Tests
+{
+    public class NewsServiceTests
+    {
+        [Fact]
+        public async Task CheckIfDeleteMethodReturnCorrectCountOfNews()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<MyForumDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var context = new MyForumDbContext(optionBuilder.Options);
+            var newsService = new NewsService(context);
+
+            var news = new News
+            {
+                Id = "123456",
+                Name = "some name",
+                Content = "some content",
+                ImageUrl = "some img",
+                CreatedOn = DateTime.UtcNow,
+                CreatorId = "1122"
+            };
+
+            await context.News.AddAsync(news);
+            await context.SaveChangesAsync();
+
+            await newsService.Delete("123456");
+
+            var expected = 0;
+            var actual = await newsService.GetAll().CountAsync();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
