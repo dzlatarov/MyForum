e5b0f53317ad45a7613dc981e9162ddc5d8c4143
dzlatarov <danzlatarov1@gmail.com>
2020-04-25 13:32:39 +0300

    check if edit method changes correctly

--- MyForum.Tests/CategoriesServiceTests.cs
+++ MyForum.Tests/CategoriesServiceTests.cs
@@ -121,5 +121,51 @@ namespace MyForum.Tests
 
             Assert.Equal(expectedCount, actual);
         }
+
+        [Fact]
+        public async Task CheckIfEditMethodChangesCorrectly()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<MyForumDbContext>()
+               .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            var context = new MyForumDbContext(optionBuilder.Options);
+            var categoryService = new CategoriesService(context);
+
+            var appCategories = new List<Category>
+            {
+                new Category
+                {
+                    Id = "1122",
+                    Name = "Action",
+                    Description = "Description......",
+                    ImageUrl = "https://cdn.clipart.email/7bad7f269d0261c39d848eaeefa0d83c_filesports-and-gamessvg-wikimedia-commons_1102-1024.png"
+                },
+                new Category
+                {
+                    Id = "1123",
+                    Name = "Adventure",
+                    Description = "Some description......",
+                    ImageUrl = "https://lh3.googleusercontent.com/proxy/Se4QkcpRfAb7bLYl_KcDNTP3UBfK3a5eYfswoQQiVS-HP8HBzQxjEqhC9M2QF04AqNaxD-MZ9PTTSwdmsuj_ZW7hXhjMXyPDVbGg1bwe_4_Y3kKzGDXTm9_a"
+                }
+            };
+
+            context.Categories.AddRange(appCategories);
+            context.SaveChanges();
+
+            var category = appCategories[0];
+            var categoryId = category.Id;
+
+            var name = "Action";
+            var description = "Description......";
+            var imageUrl = "https://lh3.googleusercontent.com/proxy/Se4QkcpRfAb7bLYl_KcDNTP3UBfK3a5eYfswoQQiVS-HP8HBzQxjEqhC9M2QF04AqNaxD-MZ9PTTSwdmsuj_ZW7hXhjMXyPDVbGg1bwe_4_Y3kKzGDXTm9_a";
+
+
+            await categoryService.Edit(categoryId, "Sport", "Hello new description", "https://cdn.clipart.email/7bad7f269d0261c39d848eaeefa0d83c_filesports-and-gamessvg-wikimedia-commons_1102-1024.png");
+
+
+            Assert.True(name != category.Name);
+            Assert.True(description != category.Description);
+            Assert.True(imageUrl != category.ImageUrl);
+        }
     }
 }
